735c18addf6d115d8e143d3c120e9644afc9b860
Rui Okazaki <okazaki200226@gmail.com>
2023-08-02 00:24:02 +0900

    Create github_repo_transfer.sh

---
+++ github_repo_transfer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+user_name=""
+org_name=""
+token=""
+
+
+gh repo list $username --json name --jq '.[].name' | while read repo; do
+  curl -X POST -H "Authorization: token $token" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$username/$repo/transfer -d '{"new_owner":"'$org_name'"}'
+  echo "Transferred $repo to $orgname"
+done
+
